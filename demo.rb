c4a3b6588ad0aafb5ee7e716df8531911fce361a
amathmagnan <amathmagnan@gmail.com>
2020-11-01 20:50:16 +0100

    rappatriation fichier solution

---
+++ demo.rb
@@ -0,0 +1,11 @@
+def circle_area(radius) #radius est une variable, radius = 3
+  return 0 if radius.negative? #return arrete si le radius
+ #todo return the area of a circle base
+ return 3.14 * radius**2 #* mulitplication simple **2 exponant
+end
+
+
+puts circle_area(3) #dans la parenthese est un argument
+#affiche
+
+#puts circle_area(-1)
